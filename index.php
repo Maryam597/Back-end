f9baba803f72056384581d1169697c11895fc9f3
Maryam597 <maryam.dri@hotmail.fr>
2023-06-12 11:20:33 +0200

    All good

--- index.php
+++ index.php
@@ -5,6 +5,22 @@
 <?php include './includes/head.inc.html'; ?>
 <?php include './includes/header.inc.html'; ?>
 
-<?php
-echo "php";
 
+
+<?php include './includes/nav.html'; ?>
+
+
+<?php
+
+  if (isset($_GET['new'])) {
+
+
+    $page = $_GET['new'];
+
+    if ($page === 'data.html') {
+      include './includes/data.html';
+    } else if ($page === 'home.html') {
+      include './includes/home.html';
+    }
+
+}
