156f5509567277312694d8a14ec8fa1f8881b459
Maryam597 <maryam.dri@hotmail.fr>
2023-06-15 16:23:39 +0200

    ok

--- index.php
+++ index.php
@@ -5,31 +5,37 @@ session_start();
 <?php include './includes/head.inc.html'; ?>
 <?php include './includes/header.inc.html'; ?>
 
-<nav style="padding-left: 10px; padding-top: 20px;">
-<?php if (isset($_SESSION['table'])) : ?>
-    <a href="index.php"> <button name="homebutton" type="button" class="btn btn-dark">Home</button></a>
-<?php endif; ?>
 
-<a href="index.php?add"> <button type="button" class="btn btn-dark">Ajouter des données</button></a>
+<div class="container-fluid">
+  <div class="row">
+<nav class="col-md-3">
+  <?php if (isset($_SESSION['table'])) : ?>
+    <a href="index.php"> <button  name="homebutton" type="button" class="btn btn-dark">Home</button></a>
+  <?php endif; ?>
+
+  <a href="index.php?add"> <button type="button" class="btn btn-dark">Ajouter des données</button></a>
+
+  <?php if (!isset($_SESSION['table'])) {
+  include_once './includes/home.html'; 
+}
+?>
 </nav>
 
+
+
+<section class="col-md-9">
+
 <?php
-if (isset($_SESSION['table'])) {
-  include_once './includes/home.html';
+// if (!isset($_SESSION['table'])) {
+//   include_once './includes/home.html';
+// }
 
-}
-
-
-if (isset($_GET['index'])) {
-  include_once './includes/home.html';
-
-}
 
 
 if (isset($_GET['add'])) {
   include_once './includes/form.html';
 } elseif (isset($_POST['form'])) {
-  
+
   $table = [
     'firstname' => $_POST['firstname'],
     'lastname' => $_POST['lastname'],
@@ -41,5 +47,25 @@ if (isset($_GET['add'])) {
   echo '<h3>Données sauvegardées</h3>';
 }
 
-include './includes/footer.html';
+// $delete = filter_input(INPUT_POST, 'delete');
+// if ($delete == 1) {
+//     session_destroy();
+// }
+
+
 ?>
+</section>
+</div>
+</div>
+<?php
+
+include './includes/footer.html';
+
+?>
+
+
+
+
+
+
+
